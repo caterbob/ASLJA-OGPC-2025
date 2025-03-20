5c1d7317da550a04435b4dae2bb7a28c9491dd5f
NastyMeyers <jmeyer25@student.albany.k12.or.us>
2025-03-20 14:19:51 -0700

    Animation + HUD
    PENIS

---
+++ the-game/hud.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	# self.position.x = Player.position.x
+	self.transform.origin = Vector2(20,20)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- the-game/player.gd
+++ the-game/player.gd
@@ -4,7 +4,10 @@ extends CharacterBody2D
 const SPEED = 50.0
 
 
+@onready var animation = $AnimatedSprite2D
 
+
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
@@ -13,6 +16,8 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+
 	var direction := Input.get_axis("ui_left", "ui_right")
 	var direction2 := Input.get_axis("ui_up", "ui_down")
 	if direction and direction2:
@@ -22,11 +27,25 @@ func _physics_process(delta: float) -> void:
 	elif direction2:
 		velocity.y = direction2 * SPEED
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	elif direction:
 		velocity.x = direction * SPEED
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	#animation
+	if velocity.x > 0:
+		animation.play("walkRight")
+	elif velocity.x < 0:
+		animation.play("walkLeft")
+	elif velocity.y > 0:
+		animation.play("walkDown")
+	elif velocity.y < 0:
+		animation.play("walkUp")
+	else:
+		animation.stop()
+	
 	move_and_slide()
