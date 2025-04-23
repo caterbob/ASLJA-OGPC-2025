ac4920db4c469b87938f685cd9f453827f331ade
NastyMeyers <jmeyer25@student.albany.k12.or.us>
2025-04-23 13:25:59 -0700

    new stuff

--- the-game/Scenes/interaction_field.gd
+++ the-game/Scenes/interaction_field.gd
@@ -1,12 +1,16 @@
-extends Label
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.visible = true
-
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	$"Interact text".visible = true
+	print("I hate my life")
