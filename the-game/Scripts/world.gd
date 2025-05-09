c816e68257ef16ecf75b8b5337fcead5dd780c0a
caterbob <98137018+caterbob@users.noreply.github.com>
2025-05-09 14:48:09 -0700

    I'm trying

--- the-game/Scripts/world.gd
+++ the-game/Scripts/world.gd
@@ -1,8 +1,9 @@
 extends Node2D
-
+var isFactory
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	isFactory = false
 	self.set_process(false)
 	self.visible = false
 
@@ -10,12 +11,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("space"):
-		var outside = get_node("/root/Outside")
-		outside.set_process(true)
-		outside.visible = true
-		for i in outside.get_children():
-			i.set_process(true)
-		for i in self.get_children():
-			i.set_process(false)
-		self.visible = false
-		self.set_process(false)
+		self.switchToOutside()
+
+
+func switchToOutside() -> void:
+	isFactory = false
+	var outside = get_node("/root/Outside")
+	outside.set_process(true)
+	outside.visible = true
+	for i in outside.get_children():
+		i.set_process(true)
+	for i in self.get_children():
+		i.set_process(false)
+	self.visible = false
+	self.set_process(false)
